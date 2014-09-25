4c7281dd0c33cc4957e03594d216ec6ff800e393
daddyderek <daddyderekahn@gmail.com>
2014-09-25 12:15:27 -1000

    finished ex 11

---
+++ promise-it-wont-hurt/ex11-do_some_work/solution.js
@@ -0,0 +1,25 @@
+var Qhttp = require('q-io/http');
+
+Qhttp.read("http://localhost:7000/")
+  .then(function(id) {
+    return Qhttp.read("http://localhost:7001/"+id);
+  })
+  .then(function(json) {
+    console.log(JSON.parse(json));
+  })
+  .then(null, console.error)
+  .done();
+
+
+/* ANSWER */
+// var qhttp = require('q-io/http');
+
+// qhttp.read("http://localhost:7000/")
+//   .then(function(id) {
+//     return qhttp.read("http://localhost:7001/" + id);
+//   })
+//   .then(function(json) {
+//     console.log(JSON.parse(json));
+//   })
+//   .then(null, console.error)
+//   .done();
