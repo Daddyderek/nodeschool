cc47b7735791d8cff8506cbd648014579a3f004f
daddyderek <daddyderekahn@gmail.com>
2014-09-24 23:23:14 -1000

    finished ex3

---
+++ promise-it-wont-hurt/ex3-to_reject_or_not_to_reject/solution.js
@@ -0,0 +1,15 @@
+var q = require('q');
+var def = q.defer();
+
+def.promise.then(console.log, console.log);
+
+def.resolve("I FIRED");
+def.reject("I DID NOT FIRE");
+
+/* ANSWER */
+// var q = require('q'),
+//   def = q.defer();
+
+// def.promise.then(console.log, console.log);
+// def.resolve("I FIRED");
+// def.reject("I DID NOT FIRE");
