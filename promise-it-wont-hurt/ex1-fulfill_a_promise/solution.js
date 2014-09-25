676bae4b3ecc69fe8296af6952ffdeb0409a4ab7
daddyderek <daddyderekahn@gmail.com>
2014-09-24 23:14:58 -1000

    finished ex2

--- promise-it-wont-hurt/ex1-fulfill_a_promise/solution.js
+++ promise-it-wont-hurt/ex1-fulfill_a_promise/solution.js
@@ -4,3 +4,10 @@ var defer = q.defer();
 defer.promise.then(console.log);
 
 setTimeout(defer.resolve, 300, 'RESOLVED!');
+
+/* ANSEWR */
+// var q = require('q');
+// var der = q.defer();
+
+// def.promise.then(console.log());
+// setTimeout(def.resolve, 300, "RESOLVED!");
