c8bda38e8e7c706bba0498e9cfb6384f27162050
daddyderek <daddyderekahn@gmail.com>
2014-09-24 23:42:38 -1000

    finished ex5

---
+++ promise-it-wont-hurt/ex5-values_and_promises/solution.js
@@ -0,0 +1,26 @@
+var q = require('q'),
+  defer = q.defer();
+
+defer.promise
+  .then(attachTitle)
+  .then(console.log);
+
+defer.resolve('MANHATTAN');
+
+function attachTitle() {
+  return "DR. " + arguments[0];
+}
+
+/* ANSWER */
+// var q = require('q'),
+//   def = q.defer();
+
+// function attachTitle(name) {
+//   return "DR. " + name;
+// }
+
+// def.promise
+//   .then(attachTitle)
+//   .then(console.log);
+
+// def.resolve("MANHATTAN");
