876fb2259fded510a0f607ce8e021bb51e7debe2
daddyderek <daddyderekahn@gmail.com>
2014-09-25 00:00:00 -1000

    finished ex7

---
+++ promise-it-wont-hurt/ex7-using_qfcall/solution.js
@@ -0,0 +1,24 @@
+var q = require('q');
+
+function parsePromised(json) {
+  var def = q.defer(),
+    result;
+  try {
+    result = JSON.parse(json);
+  } catch (e) {
+    def.reject(e);
+  }
+  def.resolve(result);
+  return q.fcall(function() {
+    return def.promise;
+  });
+}
+
+parsePromised(process.argv[2])
+  .then(null, console.log);
+
+/* ANSEWR */
+// var q = require('q');
+
+// q.fcall(JSON.parse, process.argv[2])
+//   .then(null, console.log);
