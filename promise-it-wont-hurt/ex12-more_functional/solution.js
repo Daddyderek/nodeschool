9dda61c19a02cef435aaea943c9559529ea9ce42
daddyderek <daddyderekahn@gmail.com>
2014-09-25 12:29:05 -1000

    finished ex12

---
+++ promise-it-wont-hurt/ex12-more_functional/solution.js
@@ -0,0 +1,26 @@
+var Qhttp = require('q-io/http'),
+  _ = require('lodash'),
+  cachePath = "http://localhost:7000/",
+  dbPath = "http://localhost:7001/";
+
+var buildDBpath = _.bind(String.prototype.concat, dbPath);
+
+Qhttp.read(cachePath)
+  .then(_.compose(Qhttp.read, buildDBpath))
+  .then(_.compose(console.log, JSON.parse))
+  .then(null, console.error)
+  .done();
+
+/* ANSWER */
+// var qhttp = require('q-io/http'),
+//   _ = require('lodash'),
+//   cachePath = "http://localhost:7000/",
+//   dbPath = "http://localhost:7001/";
+
+// var buildDbPath = _.bind(String.prototype.concat, dbPath);
+
+// qhttp.read(cachePath)
+//   .then(_.compose(qhttp.read, buildDbPath))
+//   .then(_.compose(console.log, JSON.parse))
+//   .then(null, console.error)
+//   .done();
