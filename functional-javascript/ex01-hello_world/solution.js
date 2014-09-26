fafda173f1129fbf73c3f9c9b8fbbfd89d3faaf3
daddyderek <daddyderekahn@gmail.com>
2014-09-25 16:40:31 -1000

    finished ex1

---
+++ functional-javascript/ex01-hello_world/solution.js
@@ -0,0 +1,10 @@
+function upperCaser(input) {
+  return input.toUpperCase();
+}
+
+module.exports = upperCaser;
+
+/* ANSWER */
+// module.exports = function(input) {
+//   return input.toUpperCase()
+// }
