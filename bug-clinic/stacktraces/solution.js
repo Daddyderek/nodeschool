c2b10c9233e33ea719c527a6ab5ddccab7370840
Derek Ahn <git.derek@gmail.com>
2017-03-02 22:16:59 -0800

    Complete bug-clinic stacktraces/traces exercise

---
+++ bug-clinic/stacktraces/solution.js
@@ -0,0 +1,20 @@
+require('stackup');
+
+const domain = require('domain');
+const readFile = require('fs').readFile;
+
+module.exports = scenario;
+
+function scenario(jsonPath, cb) {
+  const d = domain.create();
+
+  d.on('error', cb);
+
+  d.run(() => {
+    readFile(jsonPath, {
+      encoding: 'utf8'
+    }, (error, contents) => {
+      cb(error, JSON.parse(contents));
+    });
+  });
+}
