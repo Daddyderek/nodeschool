99f4583f9a9c099f9b1c75273b7cffde40352c31
Derek Ahn <git.derek@gmail.com>
2017-03-04 22:37:11 -0800

    Update trace/probe exercise

--- bug-clinic/trace/solution.js
+++ bug-clinic/trace/solution.js
@@ -1,15 +1,15 @@
-const createServer = require("http").createServer;
-const trace        = require("jstrace");
+const createServer = require('http').createServer;
+const trace        = require('jstrace');
 
 createServer((req, res) => {
-  trace("request:start", {
+  trace('request:start', {
     url: req.url
   });
 
-  res.setHeader("content-type", "application/json");
+  res.setHeader('content-type', 'application/json');
 
   let status, body;
-  if (req.url === "/prognosis" && req.method === "GET") {
+  if (req.url === '/prognosis' && req.method === 'GET') {
     status = 200;
     body = {
       ok: true
@@ -17,18 +17,18 @@ createServer((req, res) => {
   } else {
     status = 404;
     body = {
-      error: "notfound"
+      error: 'notfound'
     };
   }
 
   res.writeHead(status);
   res.end(JSON.stringify(body));
 
-  trace("request:end", {
+  trace('request:end', {
     statusCode: status,
     body: body
   });
 })
 .listen(5000, () => {
-  console.error("up");
+  console.error('up');
 });
