360ecfbdbf393d1372ed0f7df192798e8b22d2e1
Derek Ahn <git.derek@gmail.com>
2017-04-29 21:44:37 -0700

    Updated solution

--- test-anything/ex03-tape-it-together/solution.js
+++ test-anything/ex03-tape-it-together/solution.js
@@ -1,9 +1,9 @@
-const test    = require('tape');
+const test = require('tape');
 const fancify = require(process.argv[2]);
 
 test('fancify', assert => {
   assert.equal(fancify('Wat'), '~*~Wat~*~', 'Wraps a string in ~*~');
-  assert.equal(fancify('Wat', false), '~*~WAT~*~', 'Optionally makes it allcaps');
-  assert.equal(fancify('Wat', true, '%'), '~%~Wat~%~', 'Optionally allows to set the character');
+  assert.equal(fancify('Wat', true), '~*~WAT~*~', 'Optionally makes it allcaps');
+  assert.equal(fancify('Wat', false, '%'), '~%~Wat~%~', 'Optionally allows to set the character');
   assert.end();
 });
