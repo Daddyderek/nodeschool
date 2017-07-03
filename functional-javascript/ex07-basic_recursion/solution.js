a0c65d1a764935dec96939598f2656a746eac4ea
Derek Ahn <git.derek@gmail.com>
2017-07-03 14:32:37 -0700

    Completed fn-js ex07 recurision

--- functional-javascript/ex07-basic_recursion/solution.js
+++ functional-javascript/ex07-basic_recursion/solution.js
@@ -1,1 +1,7 @@
-module.exports = 
+module.exports = (arr, fn, initial) => (function reduce(index, value) {
+  if (index > arr.length -1) {
+    return value;
+  }
+  return reduce(index+1, fn(value, arr[index], index, arr));
+}(0, initial));
+
